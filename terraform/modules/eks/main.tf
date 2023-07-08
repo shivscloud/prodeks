65fde59eb2b4ed9e66663c4b317c8ecd1dc8c924
shivasingam111 <shivasingam111@gmail.com>
2023-07-09 00:00:17 +0530

    changeing subnet

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -32,6 +32,8 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  map_public_ip_on_launch = True
+  instance_types = ["t2.small"]
   subnet_ids      = var.subnet_ids
   scaling_config {
     desired_size = 1
@@ -39,6 +41,8 @@ resource "aws_eks_node_group" "eks_node_group" {
     max_size     = 1
   }
 }
+
+
 
 resource "aws_iam_role" "eks_node_group_role" {
   name = "my-eks-node-group-role"
