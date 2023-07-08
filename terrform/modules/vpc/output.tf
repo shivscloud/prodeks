6f40fa682666d6c76a9908be17986177ac9ee921
shivasingam111 <shivasingam111@gmail.com>
2023-07-08 22:56:30 +0530

    Changing the foldername

--- terrform/modules/vpc/output.tf
+++
@@ -1,7 +0,0 @@
-output "subnet_ids" {
-  value = values(aws_subnet.private_subnet)[*].id
-}
-
-output "vpc_id" {
-  value = aws_vpc.my_vpc.id
-}
--- terrform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-az = ["eu-west-1a", "eu-west-1b", "eu-west-1c"] 
-ami_id = "ami-09f6caae59175ba13"
-
